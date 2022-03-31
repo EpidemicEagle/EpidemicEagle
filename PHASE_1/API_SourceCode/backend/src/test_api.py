3e20a29a40e13b8be6916d9d69e5b718d8b1a81e
Aaron <aashekau@gmail.com>
2022-03-31 11:05:08 +1100

    Fixed another minor issue

--- PHASE_1/API_SourceCode/backend/src/test_api.py
+++ PHASE_1/API_SourceCode/backend/src/test_api.py
@@ -12,15 +12,15 @@ b = "2018-11-01 xx:xx:xx"
 c = "2018-11-xx 17:00:xx"
 
 
-# /api/articles
-# /api/articles/{article_id}
-# /api/reports
-# /api/reports/{report_id}
-# /api/search
+# /api/v1/articles
+# /api/v1/articles/{article_id}
+# /api/v1/reports
+# /api/v1/reports/{report_id}
+# /api/v1/search
 
 def test_search():
     params = {"start_date":"2018-xx-xx xx:xx:xx", "end_date":"2018-xx-xx xx:xx:xx", "key_terms":"wow,no", "location":"somewhere"}
-    response = client.get("/api/search", params=params)
+    response = client.get("/api/v1/search", params=params)
     assert response.status_code == 200
     # assert keys exist in the response
     assert {'page_number', 'num_pages', 'results'} == set(response.json().keys())
@@ -28,41 +28,41 @@ def test_search():
 
 def test_report():
     params = {"start_date":"2018-xx-xx xx:xx:xx", "end_date":"2018-xx-xx xx:xx:xx", "key_terms":"wow,no", "location":"somewhere"}
-    response = client.get("/api/reports", params=params)
+    response = client.get("/api/v1/reports", params=params)
     assert response.status_code == 200
     # assert keys exist in the response
     assert {'page_number', 'num_pages', 'reports'} == set(response.json().keys())
 
 def test_report_id():
     id = 1
-    response = client.get(f"/api/reports/{id}")
+    response = client.get(f"/api/v1/reports/{id}")
     assert response.status_code == 200
 
 def test_report_id_too_big():
     id = 1200
-    response = client.get(f"/api/reports/{id}")
+    response = client.get(f"/api/v1/reports/{id}")
     assert response.status_code == 404
 
 def test_article():
     params = {"start_date":"2018-xx-xx xx:xx:xx", "end_date":"2018-xx-xx xx:xx:xx", "key_terms":"wow,no", "location":"somewhere"}
-    response = client.get("/api/articles", params=params)
+    response = client.get("/api/v1/articles", params=params)
     assert response.status_code == 200
     # assert keys exist in the response
     assert {'page_number', 'num_pages', 'articles'} == set(response.json().keys())
 
 def test_article_id():
     id = 1
-    response = client.get(f"/api/articles/{id}")
+    response = client.get(f"/api/v1/articles/{id}")
     assert response.status_code == 200
 
 def test_article_id_too_big():
     id = 1200
-    response = client.get(f"/api/articles/{id}")
+    response = client.get(f"/api/v1/articles/{id}")
     assert response.status_code == 404
 
 def test_search():
     params = {"start_date":"2018-xx-xx xx:xx:xx", "end_date":"2018-xx-xx xx:xx:xx", "key_terms":"wow,no", "location":"somewhere"}
-    response = client.get("/api/search", params=params)
+    response = client.get("/api/v1/search", params=params)
     assert response.status_code == 200
     # assert keys exist in the response
     assert {'page_number', 'num_pages', 'results'} == set(response.json().keys())
