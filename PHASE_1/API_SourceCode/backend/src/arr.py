b7d0b8043ae42d456e2969018b6b61bd777da4e5
Aaron <aashekau@gmail.com>
2022-04-02 23:30:35 +1100

    changed some files

--- PHASE_1/API_SourceCode/backend/src/arr.py
+++ PHASE_1/API_SourceCode/backend/src/arr.py
@@ -1,11 +1,20 @@
 import json 
 
-f = open('disease_list.json')
+f = open('articles.json')
 
-js = json.load(f)
-l = []
+js = json.load(f)['articles']
+
+v = 0
+r = 0
 for i in js:
-    print(i['name'])
-    l.append(i['name'])
+    i['articleId'] = v
+    for rp in i ['reports']:
+        rp['reportId'] = r
+        r+= 1
+    # print(i)
+    v+= 1
 
-print(l)
+
+a_file = open("sample_file.json", "w")
+json.dump(js, a_file)
+a_file.close()
